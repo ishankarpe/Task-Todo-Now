c9594f152d35dc6e34e1263df38f9bdec8db3530
Eshan <eshaankarpe@gmail.com>
2020-11-14 14:02:35 -0800

    Followed Angela's code #2

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:todonow_flutter/widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
-  Widget buildBottomSheet(BuildContext context) {
-    return Container();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +14,8 @@ class TasksScreen extends StatelessWidget {
           color: Colors.black,
         ),
         onPressed: () {
-          showModalBottomSheet(context: context, builder: buildBottomSheet);
+          showModalBottomSheet(
+              context: context, builder: (context) => Container());
         },
       ),
       body: Column(
