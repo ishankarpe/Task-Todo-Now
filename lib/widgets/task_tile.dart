eaad1a2750d34f2d8bc9bf1189808b69cfe99106
Eshan <eshaankarpe@gmail.com>
2020-11-22 09:49:03 -0800

    Followed Angela's code #4

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -5,10 +5,28 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('Task 1'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool value) {},
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  @override
+  _TaskCheckboxState createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.black,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue;
+        });
+      },
     );
   }
 }
