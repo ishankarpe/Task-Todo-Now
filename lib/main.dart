e29542b2e91f342eabe77862e54281566557d9ff
Eshan Karpe <67484673+eshankarpe@users.noreply.github.com>
2020-12-04 17:17:44 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+
 import 'package:flutter/material.dart';
-import 'package:todonow_flutter/screens/tasks_screen.dart';
+import 'package:task_todo_now_flutter/screens/tasks_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:task_todo_now_flutter/models/task_data.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TasksScreen(),
+    return ChangeNotifierProvider(
+      builder: (context) => TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
     );
   }
 }
