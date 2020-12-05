fa993a25df83d4332d280d7b5e3eeab7949ea5d6
eshankarpe <eshaankarpe@gmail.com>
2020-12-04 19:40:48 -0800

    Completed App with no errors

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -4,9 +4,6 @@ import 'dart:collection';
 
 class TaskData extends ChangeNotifier {
   List<Task> _tasks = [
-    Task(name: 'Buy milk'),
-    Task(name: 'Buy eggs'),
-    Task(name: 'Buy bread'),
   ];
 
   UnmodifiableListView<Task> get tasks {
