53230167c5fcad1e34495518a8122ec5a1e77bfc
Eshan Karpe <67484673+eshankarpe@users.noreply.github.com>
2020-12-04 17:27:01 -0800

    Update tasks_list.dart

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,31 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:todonow_flutter/models/task.dart';
-import 'package:todonow_flutter/widgets/task_tile.dart';
+import 'package:task_todo_now_flutter/widgets/task_tile.dart';
+import 'package:provider/provider.dart';
+import 'package:task_todo_now_flutter/models/task_data.dart';
 
-class TasksList extends StatefulWidget {
-
-  final List<Task> tasks;
-
-  TasksList(this.tasks);
-  @override
-  _TasksListState createState() => _TasksListState();
-}
-
-class _TasksListState extends State<TasksList> {
-
-
+class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index){
-      return TaskTile(taskTitle: widget.tasks[index].name,
-        isChecked: widget.tasks[index].isDone,
-            checkboxCallback: (bool checkboxState) {
-          setState(() {
-            widget.tasks[index].toggleDone() ;
-          });
-        },
-      );
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkboxCallback: (checkboxState) {
+                taskData.updateTask(task);
+              },
+              longPressCallback: () {
+                taskData.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskData.taskCount,
+        );
       },
-    itemCount: widget.tasks.length ,);
+    );
   }
 }
