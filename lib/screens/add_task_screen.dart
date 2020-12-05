7ac52262821392b03573bce0cf776e9343b448d3
Eshan Karpe <67484673+eshankarpe@users.noreply.github.com>
2020-12-04 17:21:29 -0800

    Update add_task_screen.dart

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,15 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
+import 'package:provider/provider.dart';
+import 'package:task_todo_now_flutter/models/task_data.dart';
 
-class AddTaskScreen extends StatefulWidget {
-  @override
-  _AddTaskScreenState createState() => _AddTaskScreenState();
-}
-
-class _AddTaskScreenState extends State<AddTaskScreen> {
+class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String newTaskTitle;
+
     return Container(
       color: Color(0xff757575),
       child: Container(
@@ -17,7 +14,9 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
         decoration: BoxDecoration(
           color: Colors.white70,
           borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -25,20 +24,29 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
             Text(
               'Add Task',
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 30.0, color: Colors.green.shade600),
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.green.shade600,
+              ),
             ),
             TextField(
               autofocus: true,
               textAlign: TextAlign.center,
+              onChanged: (newText) {
+                newTaskTitle = newText;
+              },
             ),
             FlatButton(
               child: Text(
                 'Add',
-                style: TextStyle(color: Colors.black),
+                style: TextStyle(
+                  color: Colors.black,
+                ),
               ),
               color: Colors.red.shade800,
               onPressed: () {
-                //Add Task To List
+                Provider.of<TaskData>(context).addTask(newTaskTitle);
+                Navigator.pop(context);
               },
             ),
           ],
